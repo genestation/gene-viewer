eb63023a03eeba69f0c172a925ac9daaea5e1f6e
Mara Kim <mara@autochthe.net>
2016-11-28 22:07:23 -0600

    Working

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,24 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './main.scss';
 
+export interface CardProps{
+	lang: string;
+	set: string;
+	cnum: number;
+}
+export interface CardState{
+	focus?: boolean;
+};
+class Card extends React.Component<CardProps,CardState> {
+	image_uri() {
+		return "https://img.scryfall.com/cards/" + this.props.lang + '/'
+		+ this.props.set + '/' + this.props.cnum + '.jpg';
+	}
+	render() {
+		return <img src={this.image_uri()} />
+	}
+}
+
 export interface MainProps{
 	autofocus?: boolean;
 }
@@ -12,6 +30,9 @@ export interface MainState{
 };
 export default class extends React.Component<MainProps,MainState> {
 	render() {
-		return <div>Hello, World!</div>
+		return <div><Card
+		lang="en"
+		set="ema"
+		cnum={49} /></div>
 	}
 }
