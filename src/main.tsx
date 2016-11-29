dbbc3e71ac6ebef405cb466eb3f8cf82a4a48489
Mara Kim <mara@autochthe.net>
2016-11-28 22:44:07 -0600

    Add card lists

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,15 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './main.scss';
 
-export interface CardProps{
+interface CardProps{
+	name: string;
 	lang: string;
 	set: string;
 	cnum: number;
 }
-export interface CardState{
+interface CardState{
 	focus?: boolean;
-};
+}
 class Card extends React.Component<CardProps,CardState> {
 	image_uri() {
 		return "https://img.scryfall.com/cards/" + this.props.lang + '/'
@@ -22,17 +23,31 @@ class Card extends React.Component<CardProps,CardState> {
 	}
 }
 
+interface CardListProps{
+	cards: CardProps[];
+}
+interface CardListState{ }
+class CardList extends React.Component<CardListProps,CardListState> {
+	render() {
+		return <ul> {
+			this.props.cards.map((card: CardProps)=>{
+				return <li>{card.name}</li>
+			})
+		} </ul>;
+	}
+}
+
 export interface MainProps{
 	autofocus?: boolean;
 }
 export interface MainState{
 	focus?: boolean;
-};
+}
 export default class extends React.Component<MainProps,MainState> {
 	render() {
-		return <div><Card
-		lang="en"
-		set="ema"
-		cnum={49} /></div>
+		return <div class="roguebuilder">
+			<Card name="Force of Will" lang="en" set="ema" cnum={49} />
+			<CardList cards={[{name: "Force of Will", lang: "en", set: "ema", cnum: 49}]}/>
+		</div>
 	}
 }
