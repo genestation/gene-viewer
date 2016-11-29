5621d55358c12fce76d9b9a64fcccf44db347bf9
Mara Kim <mara@autochthe.net>
2016-11-28 21:21:54 -0600

    Make generic

--- dev/dev.tsx
+++ dev/dev.tsx
@@ -2,40 +2,8 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Console from '../src/main.tsx';
-
-interface EchoConsoleState {
-	count: number;
-}
-class EchoConsole extends React.Component<{},EchoConsoleState> {
-	constructor(props: {}) {
-		super(props);
-		this.state = {
-			count: 0,
-		};
-	}
-	child: {
-		console?: Console,
-	} = {};
-	echo = (text: string) => {
-		this.child.console.log(text);
-		this.setState({
-			count: this.state.count+1,
-		}, this.child.console.return);
-	}
-	promptLabel = () => {
-		return this.state.count + "> ";
-	}
-	render() {
-		return <Console ref={ref => this.child.console = ref}
-			handler={this.echo}
-			promptLabel={this.promptLabel}
-			welcomeMessage={"Welcome to the react-console demo!"}
-			autofocus={true}
-		/>;
-	}
-}
+import Main from '../src/main.tsx';
 
 export function init(element: Element) {
-	ReactDOM.render(<EchoConsole/>, element);
+	ReactDOM.render(<Main/>, element);
 }
