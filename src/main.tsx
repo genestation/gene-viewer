1376a703aae38ee99cf339155b48e96d484eb9de
Mara Kim <mara@autochthe.net>
2016-11-29 01:28:20 -0600

    Dynamically fetch card info

--- src/main.tsx
+++ src/main.tsx
@@ -2,42 +2,158 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import 'whatwg-fetch'; // HOPE remove window.fetch polyfill
 import './main.scss';
 
-interface CardProps{
-	name: string;
-	lang: string;
-	set: string;
-	cnum: number;
-}
-interface CardState{
-	focus?: boolean;
-}
-class Card extends React.Component<CardProps,CardState> {
-	image_uri() {
-		return "https://img.scryfall.com/cards/" + this.props.lang + '/'
-		+ this.props.set + '/' + this.props.cnum + '.jpg';
-	}
-	render() {
-		return <img src={this.image_uri()} />
-	}
-}
-
 interface CardListProps{
-	cards: CardProps[];
+	cards: string[];
 }
 interface CardListState{ }
 class CardList extends React.Component<CardListProps,CardListState> {
 	render() {
 		let counts: {[key: string]: number} = {}
-		this.props.cards.forEach((card: CardProps)=>{
-			counts[card.name] = (counts[card.name] || 0) + 1;
+		this.props.cards.forEach((card: string)=>{
+			counts[card] = (counts[card] || 0) + 1;
 		});
 		return <ul> {
-			Object.keys(counts).map((card: string)=>{
-				return <li>{counts[card] + "x"}&nbsp;{card}</li>
+			Object.keys(counts).map((card: string, idx: number)=>{
+				return <li key={idx}>{counts[card] + "×"}&nbsp;{card}</li>
 			})
 		} </ul>;
+	}
+}
+
+interface ScryfallCard {
+	name: string;
+	mana_cost?: string;
+	converted_mana_cost: string;
+	type_line: string;
+	oracle_text: string;
+	power?: string;
+	toughness?: string;
+	loyalty?: string;
+	hand_modifier?: string;
+	life_modifier?: string;
+	colors: string[];
+	color_identity: string[];
+	layout: string;
+	legalities: {
+		standard: string;
+		modern: string;
+		legacy: string;
+		vintage: string;
+		commander: string;
+		pauper: string;
+		frontier: string;
+		penny: string;
+		duel: string;
+	};
+	reserved: boolean;
+	id: string;
+	multiverse_id?: number;
+	mtgo_id?: number;
+	set: string;
+	set_name: string;
+	collector_number?: string;
+	all_parts?: {
+		id: string;
+		name: string;
+		uri: string;
+	}[];
+	rarity: string;
+	digital: boolean;
+	flavor_text: string;
+	artist: string;
+	frame: string;
+	border: string;
+	timeshifted: boolean;
+	colorshifted: boolean;
+	futureshifted: boolean;
+	usd?: string;
+	tix?: string;
+	scryfall_uri: string;
+	image_uri: string;
+}
+interface ScryfallList {
+	data: ScryfallCard[];
+	has_more: boolean;
+	next_page?: string;
+	total_cards?: number;
+	warnings: string[];
+}
+interface DeckListProps{
+	mainboard?: string[];
+	sideboard?: string[];
+}
+interface DeckListState{
+	cardinfo?: {[key: string]: ScryfallCard};
+	curr?: string;
+	img?: string;
+}
+class DeckList extends React.Component<DeckListProps,DeckListState> {
+	static defaultProps: DeckListProps = {
+		mainboard: [],
+		sideboard: [],
+	}
+	constructor(props: DeckListProps) {
+		super(props);
+		let cardinfo: {[key: string]: ScryfallCard} = {}
+		props.mainboard.forEach((card: string)=>{
+			cardinfo[card] = null
+		});
+		props.sideboard.forEach((card: string)=>{
+			cardinfo[card] = null
+		});
+		this.state = {
+			cardinfo: cardinfo,
+			curr: props.mainboard.length > 0?props.mainboard[0]:null
+		}
+		this.updateInfo();
+	}
+	componentWillReceiveProps(nextProps: DeckListProps) {
+		let cardinfo: {[key: string]: ScryfallCard} = this.state.cardinfo
+		nextProps.mainboard.forEach((card: string)=>{
+			if(!cardinfo.hasOwnProperty(card)) {
+				cardinfo[card] = null
+			}
+		});
+		nextProps.sideboard.forEach((card: string)=>{
+			if(!cardinfo.hasOwnProperty(card)) {
+				cardinfo[card] = null
+			}
+		});
+		this.setState({
+			cardinfo: cardinfo
+		}, this.updateInfo)
+	}
+	updateInfo() {
+		let missingInfo = Object.keys(this.state.cardinfo)
+			.filter((card: string)=> { return !this.state.cardinfo[card] });
+		missingInfo.forEach((card: string)=>{
+			fetch('https://api.scryfall.com/cards/search?q='+encodeURIComponent('!"'+card+'"'))
+				.then((response: Promise<Response>)=>{
+					if(response.status !== 200) {
+						console.log(response.status, response.url);
+					} else {
+						response.json().then((json: ScryfallList)=>{
+							let info: ScryfallCard = json.data[0];
+							let state = this.state;
+							state.cardinfo[info.name] = info;
+							if(this.state.curr == info.name) {
+								state.img = info.image_uri;
+							}
+							this.setState(state);
+						})
+					}
+				})
+		})
+	}
+	render() {
+		return <div className="roguebuilder">
+			<CardList cards={this.props.mainboard}/>
+			<CardList cards={this.props.sideboard}/>
+			{this.state.img?<img src={this.state.img}/>:null}
+		</div>
 	}
 }
 
@@ -49,9 +165,6 @@ export interface MainState{
 }
 export default class extends React.Component<MainProps,MainState> {
 	render() {
-		return <div class="roguebuilder">
-			<Card name="Force of Will" lang="en" set="ema" cnum={49} />
-			<CardList cards={[{name: "Force of Will", lang: "en", set: "ema", cnum: 49}]}/>
-		</div>
+		return <DeckList mainboard={["Force of Will"]}/>
 	}
 }
