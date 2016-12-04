72050ed83ab94f1675c8d395c60fe209e23bca87
Mara Kim <mara@autochthe.net>
2016-12-04 07:10:57 -0600

    layout style

--- src/card-list.tsx
+++ src/card-list.tsx
@@ -28,7 +28,8 @@ export class CardList extends React.Component<CardListProps,CardListState> {
 			return a + b;
 		}, 0);
 		let title = this.props.title + " (" + count + ")";
-		return <div className="card-list">
+		return <div className="card-list"
+			onMouseOver={()=>this.props.setCurr(this.props.cards[0].card)} >
 			{this.props.title?(this.props.sublist?<h3 className="title">{title}</h3>:<h2 className="title">{title}</h2>):null}
 			<div className="actions" >
 				<i className="fa fa-download" aria-hidden="true" onClick={this.props.onDownload}/>
@@ -39,7 +40,7 @@ export class CardList extends React.Component<CardListProps,CardListState> {
 					// Calculate width
 					let ratio = (20/*table width*/ - 2.5/*quantity-width*/ - 1 - (mana_cost?mana_cost.length:0))/(card.length*0.5);
 					return <tr key={idx}
-						onMouseOver={()=>this.props.setCurr(card)}
+						onMouseOver={(e: SyntheticEvent)=>{e.stopPropagation(); this.props.setCurr(card)}}
 						onClick={()=>{this.props.setCurr(card); this.props.showPreview()}} >
 						<td className="quantity">{count + "×"}</td>
 						<td>
