b2616c53ee2eff0684d71838f3a2a82037e90c37
Mara Kim <mara@autochthe.net>
2016-12-08 01:29:40 -0600

    Partial

--- src/card-stack.tsx
+++ src/card-stack.tsx
@@ -15,17 +15,18 @@ export class CardStack extends React.Component<CardStackProps,CardStackState> {
 		tapped: 0
 	};
 	render() {
-		let stack: JSX.Element[] = [];
+		let tapped: JSX.Element[] = [];
+		let untapped: JSX.Element[] = [];
 		for(let i = 0; i < this.props.count - this.state.tapped; i++) {
-			stack.push(<div key={i}
-				className="deck-player-card deck-player-card-stack-item"
+			untapped.push(<div key={i}
+				className="deck-player-card deck-player-card-stack-item deck-player-card-stack-item-untapped"
 				onClick={()=>this.setState({tapped: this.state.tapped+1})}
 				>
 				<img className="deck-player-card-img" src={CardInfo.image(this.props.card)}/>
 			</div>);
 		}
 		for(let i = this.props.count - this.state.tapped; i < this.props.count; i++) {
-			stack.push(<div key={i}
+			tapped.push(<div key={i}
 			className="deck-player-card deck-player-card-stack-item deck-player-card-stack-item-tapped"
 				onClick={()=>this.setState({tapped: this.state.tapped-1})}
 				>
@@ -33,7 +34,12 @@ export class CardStack extends React.Component<CardStackProps,CardStackState> {
 			</div>);
 		}
 		return <div className="deck-player-card-stack">
-			{stack}
+			<div className="deck-player-card-stack-untapped">
+				{untapped}
+			</div>
+			<div className="deck-player-card-stack-tapped">
+				{tapped}
+			</div>
 		</div>
 	}
 }
