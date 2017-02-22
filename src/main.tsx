150dfd335d1ed6b2613e14f3846e0f69726674df
Mara Kim <mara@autochthe.net>
2017-02-22 00:23:47 -0600

    Basic gene locs

--- src/main.tsx
+++ src/main.tsx
@@ -19,10 +19,12 @@ interface Feature {
 }
 
 interface ViewerShape {
-	viewWidth: number,
-	viewHeight: number,
-	minY: number,
+	viewStart: number,
+	dnaY: number,
 	dnaHeight: number,
+	strandHeight: number,
+	plusStrandY: number,
+	minusStrandY: number,
 }
 
 interface GenomeFeatureProps {
@@ -36,25 +38,39 @@ class GenomeFeature extends React.Component<GenomeFeatureProps,{}> {
 	}
 	render():JSX.Element {
 		switch(this.props.feature.ftype) {
-			case 'gene':
-			case 'mRNA':
-				return <g>
-					<rect /> //TODO viewport height highlight this.props.feature.loc
-					{ this.props.feature.child.map((feature: Feature)=>{
-						return <GenomeFeature shape={this.props.shape} feature={feature}/>
-					}) }
-				</g>;
-			default:
-				return <g>
-				{ this.props.feature.loc.map((loc: Location)=>{
-						return <rect />
+		case 'gene':
+		case 'mRNA':
+			return <g>
+				<rect /> //TODO viewport height highlight this.props.feature.loc
+				{ this.props.feature.child.map((feature: Feature)=>{
+					return <GenomeFeature shape={this.props.shape} feature={feature}/>
 				}) }
-				{
+			</g>;
+		default:
+			return <g>
+				{this.props.feature.loc?
+					this.props.feature.loc.map((loc: Location)=>{
+						const rectX = loc.start - this.props.shape.viewStart;
+						const rectWidth = loc.end - loc.start;
+						switch(loc.strand) {
+						case 1:
+							return <rect x={rectX} y={this.props.shape.plusStrandY}
+								width={rectWidth} height={this.props.shape.strandHeight}/>
+						case -1:
+							return <rect x={rectX} y={this.props.shape.minusStrandY}
+								width={rectWidth} height={this.props.shape.strandHeight}/>
+						default:
+							return <rect x={rectX} y={this.props.shape.dnaY}
+								width={rectWidth} height={this.props.shape.dnaHeight}/>
+						}
+					})
+				:null}
+				{this.props.feature.child?
 					this.props.feature.child.map((feature: Feature)=>{
 						return <GenomeFeature shape={this.props.shape} feature={feature}/>
 					})
-				}
-				</g>;
+				:null}
+			</g>;
 		}
 	}
 }
@@ -80,19 +96,26 @@ export default class GeneViewer extends React.Component<GeneViewerProps,GeneView
 		}
 	}
 	render() {
+		const viewWidth = this.state.viewEnd - this.state.viewStart;
 		const viewHeight = 100;
+		const minY = -viewHeight/2;
+		const dnaHeight = 10;
+		const strandHeight = 4;
+		const dnaY = -dnaHeight/2;
 		const shape = {
-			viewWidth: this.state.viewEnd - this.state.viewStart,
-			viewHeight: viewHeight,
-			minY: -viewHeight/2,
-			dnaHeight: 10,
+			viewStart: this.state.viewStart,
+			dnaY: dnaY,
+			dnaHeight: dnaHeight,
+			strandHeight: strandHeight,
+			plusStrandY: dnaY,
+			minusStrandY: dnaY+dnaHeight-strandHeight,
 		};
 		return <div className="geneviewer">
 			<svg width="100%" height="100%"
-			 viewBox={"0 "+shape.minY+" "+shape.viewWidth+" "+shape.viewHeight}
+			 viewBox={"0 "+minY+" "+viewWidth+" "+viewHeight}
 			 preserveAspectRatio="none">
-				<rect x="0" y={-shape.dnaHeight/2}
-				 width={shape.viewWidth} height={shape.dnaHeight}
+				<rect x="0" y={dnaY}
+				 width={viewWidth} height={dnaHeight}
 				 style={{fill:"#8b96a8"}} />
 				{ this.props.features.map((feature: Feature)=>{
 					return <GenomeFeature shape={shape} feature={feature} />
