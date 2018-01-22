1ad840864dd1af3efdb1a4a8fa3e3e8471c14fdc
Mara Kim <mara.kim@vanderbilt.edu>
2018-01-22 17:08:31 -0600

    Style

--- src/TreeSelect.tsx
+++ src/TreeSelect.tsx
@@ -1,7 +1,7 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {Treebeard} from 'react-treebeard';
+import {Treebeard, theme} from 'react-treebeard';
 
 export interface TreeNode {
 	name: string,
@@ -52,6 +52,80 @@ export class TreeSelect extends React.Component<TreeSelectProps,TreeSelectState>
 		return <Treebeard
 			data={fields}
 			onToggle={this.selectNode}
+			style={TreebeardTheme}
 		/>
 	}
 }
+
+let TreebeardTheme = {
+    tree: {
+        base: {
+            listStyle: 'none',
+            margin: 0,
+            padding: 0,
+        },
+        node: {
+            base: {
+                position: 'relative'
+            },
+            link: {
+                cursor: 'pointer',
+                position: 'relative',
+                padding: '0px 5px',
+                display: 'block'
+            },
+            activeLink: {
+                background: '#c5c5c5'
+            },
+            toggle: {
+                base: {
+                    position: 'relative',
+                    display: 'inline-block',
+                    verticalAlign: 'top',
+                    marginLeft: '-5px',
+                    height: '24px',
+                    width: '24px'
+                },
+                wrapper: {
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    margin: '-7px 0 0 -7px',
+                    height: '14px'
+                },
+                height: 14,
+                width: 14,
+                arrow: {
+                    fill: 'black',
+                    strokeWidth: 0
+                }
+            },
+            header: {
+                base: {
+                    display: 'inline-block',
+                    verticalAlign: 'top',
+                },
+                connector: {
+                    width: '2px',
+                    height: '12px',
+                    borderLeft: 'solid 2px black',
+                    borderBottom: 'solid 2px black',
+                    position: 'absolute',
+                    top: '0px',
+                    left: '-21px'
+                },
+                title: {
+                    lineHeight: '24px',
+                    verticalAlign: 'middle'
+                }
+            },
+            subtree: {
+                listStyle: 'none',
+                paddingLeft: '19px'
+            },
+            loading: {
+                color: '#E2C089'
+            }
+        }
+    }
+};
