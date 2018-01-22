5585bac09190ed24d63487a684bb706a54e0f693
Mara Kim <mara.kim@vanderbilt.edu>
2018-01-22 15:05:36 -0600

    Update filtering

--- src/TreeSelect.tsx
+++ src/TreeSelect.tsx
@@ -10,19 +10,22 @@ export interface TreeNode {
 	toggled?: boolean,
 	active?: boolean,
 	loading?: boolean,
-	valid?: boolean,
 }
 interface TreeSelectProps {
 	fields: string[],
+	value: string,
 	onSelect: (node: TreeNode)=>any,
 }
-interface TreeSelectState {
-	fields?: TreeNode[],
-	cursor?: TreeNode,
-}
+interface TreeSelectState {}
 export class TreeSelect extends React.Component<TreeSelectProps,TreeSelectState> {
 	constructor(props: TreeSelectProps) {
 		super(props);
+		this.state = {};
+	}
+	selectNode = (node: TreeNode)=>{
+		this.props.onSelect(node);
+	}
+	render() {
 		let fields = [] as TreeNode[];
 		this.props.fields.forEach((field: string)=>{
 			let ptr = fields;
@@ -32,11 +35,11 @@ export class TreeSelect extends React.Component<TreeSelectProps,TreeSelectState>
 					childIdx = ptr.push({
 						name: part,
 						path: array.slice(0,idx+1).join('.'),
+						toggled: this.props.value && this.props.value.startsWith(array.slice(0,idx+1).join('.')),
+						active: this.props.value && this.props.value == array.slice(0,idx+1).join('.'),
 					}) - 1;
 				}
-				if(idx == array.length - 1) {
-					ptr[childIdx].valid = true;
-				} else {
+				if(idx != array.length - 1) {
 					if(!ptr[childIdx].children) {
 						ptr[childIdx].children = [];
 					}
@@ -44,46 +47,9 @@ export class TreeSelect extends React.Component<TreeSelectProps,TreeSelectState>
 				}
 			})
 		});
-		this.state = {
-			fields: fields,
-		};
-	}
-	handleToggle = (node: any, toggled: boolean)=>{
-		let ptr = this.state.fields;
-		if(this.state.cursor) {
-			this.state.cursor.path.split('.').forEach((part: string, idx: number, array: string[])=>{
-				ptr.forEach((child: TreeNode)=>{
-					if (child.name == part) {
-						child.toggled = false;
-					}
-				});
-				if(idx < array.length - 1) {
-					ptr = ptr.find((child: TreeNode)=>child.name == part).children;
-				}
-			});
-			this.state.cursor.active = false;
-			ptr = this.state.fields;
-		}
-		node.path.split('.').forEach((part: string, idx: number, array: string[])=>{
-			ptr.forEach((child: TreeNode)=>{
-				if (child.name == part) {
-					child.toggled = true;
-				}
-			});
-			if(idx < array.length - 1) {
-				ptr = ptr.find((child: TreeNode)=>child.name == part).children;
-			}
-		});
-		node.active = true;
-		this.setState({
-			fields: this.state.fields,
-			cursor: node,
-		},()=>this.props.onSelect(node));
-	}
-	render() {
 		return <Treebeard
-			data={this.state.fields}
-			onToggle={this.handleToggle}
+			data={fields}
+			onToggle={this.selectNode}
 		/>
 	}
 }
