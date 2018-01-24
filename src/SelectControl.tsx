49c7f2757e0b1ae4c220ac48d2b064f1422398da
Mara Kim <mara.kim@vanderbilt.edu>
2018-01-24 01:05:11 -0600

    Remove unnecessary group

--- src/SelectControl.tsx
+++ src/SelectControl.tsx
@@ -47,28 +47,26 @@ export class SelectControl extends React.Component<SelectControlProps,SelectCont
 	}
 	render() {
 		return <div className="selectcontrol-container">
-				<Dropdown className="selectcontrol-element" autoclose={false} label="View"
-					value={this.props.value.view?this.props.value.view:"<auto>"}>
-					<TreeSelect fields={this.props.fields} value={this.props.value.view}
-						onSelect={(node: TreeNode)=>{this.handleChange({view: node.path})}} />
-				</Dropdown>
-			<div className="selectcontrol-container-group">
-				<Dropdown className="selectcontrol-element" autoclose={false} label="Filter"
-					value={this.props.value.filter?this.props.value.filter:"<associations>"}>
-					<TreeSelect fields={this.props.fields} value={this.props.value.filter}
-						onSelect={(node: TreeNode)=>{this.handleChange({filter: node.path})}} />
-				</Dropdown>
-				<Dropdown className="selectcontrol-element" label="Order"
-					value={DropdownListFind(this.props.value.order,SelectControl.orderOptions).label}>
-					<DropdownList options={SelectControl.orderOptions}
-						onChange={(option: DropdownListOption)=>{this.handleChange({order: option.value})}} />
-				</Dropdown>
-				<Dropdown className="selectcontrol-element" label="Limit"
-					value={DropdownListFind(this.props.value.limit,SelectControl.limitOptions).label}>
-					<DropdownList options={SelectControl.limitOptions}
-						onChange={(option: DropdownListOption)=>{this.handleChange({limit: option.value})}} />
-				</Dropdown>
-			</div>
+			<Dropdown className="selectcontrol-element" autoclose={false} label="View"
+				value={this.props.value.view?this.props.value.view:"<auto>"}>
+				<TreeSelect fields={this.props.fields} value={this.props.value.view}
+					onSelect={(node: TreeNode)=>{this.handleChange({view: node.path})}} />
+			</Dropdown>
+			<Dropdown className="selectcontrol-element" autoclose={false} label="Filter"
+				value={this.props.value.filter?this.props.value.filter:"<associations>"}>
+				<TreeSelect fields={this.props.fields} value={this.props.value.filter}
+					onSelect={(node: TreeNode)=>{this.handleChange({filter: node.path})}} />
+			</Dropdown>
+			<Dropdown className="selectcontrol-element" label="Order"
+				value={DropdownListFind(this.props.value.order,SelectControl.orderOptions).label}>
+				<DropdownList options={SelectControl.orderOptions}
+					onChange={(option: DropdownListOption)=>{this.handleChange({order: option.value})}} />
+			</Dropdown>
+			<Dropdown className="selectcontrol-element" label="Limit"
+				value={DropdownListFind(this.props.value.limit,SelectControl.limitOptions).label}>
+				<DropdownList options={SelectControl.limitOptions}
+					onChange={(option: DropdownListOption)=>{this.handleChange({limit: option.value})}} />
+			</Dropdown>
 		</div>
 	}
 }
