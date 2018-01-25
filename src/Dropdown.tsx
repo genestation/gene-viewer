da33393dbc885ec81fc5e46fb8a502d7042e0a75
Mara Kim <mara.kim@vanderbilt.edu>
2018-01-25 01:12:32 -0600

    Better close behavior

--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -32,6 +32,9 @@ export class Dropdown extends React.Component<DropdownProps,DropdownState> {
 			tabIndex={this.props.tabindex?this.props.tabindex:0}
 			ref={(ref)=>this.child.container = ref}
 			>
+			<div className="dropdown-close"
+				onClick={()=>{this.child.container.blur()}}
+			/>
 			<div className="dropdown-label">
 				{this.props.label}:
 			</div>
@@ -41,6 +44,7 @@ export class Dropdown extends React.Component<DropdownProps,DropdownState> {
 				</div>
 				<div className="dropdown-items"
 					onClick={()=>{this.props.autoclose?this.child.container.blur():null}}
+					onMouseLeave={()=>{this.child.container.blur()}}
 				>
 					{this.props.children}
 				</div>
