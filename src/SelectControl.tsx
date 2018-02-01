38c9f117e61a3adb7ec4d0f5c4d7cc6f4c2cfa4c
Mara Kim <mara.kim@vanderbilt.edu>
2018-02-01 15:31:33 -0600

    Show click / hover controls

--- src/SelectControl.tsx
+++ src/SelectControl.tsx
@@ -53,27 +53,61 @@ export class SelectControl extends React.Component<SelectControlProps,SelectCont
 		this.props.onChange(Object.assign(this.props.value,settings));
 	}
 	render() {
+		const region = this.props.value.hoverRegion ?
+			this.props.value.hoverRegion : this.props.value.clickRegion;
+		const bucket = this.props.value.hoverBucket ?
+			this.props.value.hoverBucket : this.props.value.clickBucket;
+		const feature = this.props.value.hoverFeature ?
+			this.props.value.hoverFeature : this.props.value.clickFeature;
 		return <div className="selectcontrol-container">
-			<Dropdown className="selectcontrol-view selectcontrol-element" autoclose={false} label="View"
-				value={this.props.value.view?this.props.value.view:"<auto>"}>
-				<TreeSelect fields={this.props.fields} value={this.props.value.view}
-					onSelect={(node: TreeNode)=>{this.handleChange({view: node.path})}} />
-			</Dropdown>
-			<Dropdown className="selectcontrol-element" autoclose={false} label="Filter"
-				value={this.props.value.filter?this.props.value.filter:"<associations>"}>
-				<TreeSelect fields={this.props.fields} value={this.props.value.filter}
-					onSelect={(node: TreeNode)=>{this.handleChange({filter: node.path})}} />
-			</Dropdown>
-			<Dropdown className="selectcontrol-element" label="Order"
-				value={DropdownListFind(this.props.value.order,SelectControl.orderOptions).label}>
-				<DropdownList options={SelectControl.orderOptions}
-					onChange={(option: DropdownListOption)=>{this.handleChange({order: option.value})}} />
-			</Dropdown>
-			<Dropdown className="selectcontrol-element" label="Limit"
-				value={DropdownListFind(this.props.value.limit,SelectControl.limitOptions).label}>
-				<DropdownList options={SelectControl.limitOptions}
-					onChange={(option: DropdownListOption)=>{this.handleChange({limit: option.value})}} />
-			</Dropdown>
+			<div className="selectcontrol-row">
+				<Dropdown className="selectcontrol-view selectcontrol-element" autoclose={false} label="View"
+					value={this.props.value.view?this.props.value.view:"<auto>"}>
+					<TreeSelect fields={this.props.fields} value={this.props.value.view}
+						onSelect={(node: TreeNode)=>{this.handleChange({view: node.path})}} />
+				</Dropdown>
+				<Dropdown className="selectcontrol-element" autoclose={false} label="Filter"
+					value={this.props.value.filter?this.props.value.filter:"<associations>"}>
+					<TreeSelect fields={this.props.fields} value={this.props.value.filter}
+						onSelect={(node: TreeNode)=>{this.handleChange({filter: node.path})}} />
+				</Dropdown>
+				{this.props.value.filter?
+					<Dropdown className="selectcontrol-element" label="Order"
+						value={DropdownListFind(this.props.value.order,SelectControl.orderOptions).label}>
+						<DropdownList options={SelectControl.orderOptions}
+							onChange={(option: DropdownListOption)=>{this.handleChange({order: option.value})}} />
+					</Dropdown>
+				:null}
+				{this.props.value.filter?
+					<Dropdown className="selectcontrol-element" label="Limit"
+						value={DropdownListFind(this.props.value.limit,SelectControl.limitOptions).label}>
+						<DropdownList options={SelectControl.limitOptions}
+							onChange={(option: DropdownListOption)=>{this.handleChange({limit: option.value})}} />
+					</Dropdown>
+				:null}
+			</div>
+			<div className="selectcontrol-row">
+				{region?
+					<Dropdown className="selectcontrol-element" label="Region"
+						value={region.join('..')}>
+					</Dropdown>
+				:null}
+				{bucket?
+					<Dropdown className="selectcontrol-element" label="Bucket"
+						value={bucket
+							.map((bucket: HistogramBucket)=>
+								bucket.from.toExponential(2)+'..'+bucket.to.toExponential(2))
+							.join(', ')
+						}>
+					</Dropdown>
+				:null}
+				{feature?
+					<Dropdown className="selectcontrol-element" label="Feature"
+						value={feature}>
+					</Dropdown>
+				:null}
+				<Dropdown className="selectcontrol-placeholder" label="" value="0" />
+			</div>
 		</div>
 	}
 }
